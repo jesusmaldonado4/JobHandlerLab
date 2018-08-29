792c7b0d424df2d7e1a9d404a261bb0ed54ef968
4015c12 <4015c12@mamey.ece.uprm.edu>
2018-08-29 12:41:25 -0400

    Test1

--- src/Employee.java
+++ src/Employee.java
@@ -80,8 +80,18 @@ public class Employee {
 	 */
 	public int seniorityLevel() {
 		
-		//Add Code Here [You have to use If/Else to get graded]
-		
+		if (yearService >= 10){
+			return 3;
+		}
+		else if (yearService >= 5 && yearService < 10 ){
+			return 2 ;
+		}
+		else if (yearService >= 1 && yearService < 5 ){
+			return 1 ;
+		}
+		else if (yearService < 1) {
+			return 0 ;
+		}
 		return 0;// Temporal Return
 		
 	}
